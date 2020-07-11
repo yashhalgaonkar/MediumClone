32440e2a8fbd2d22c52e3a132bf39823ca7eac4c
Yash <yash.halgaonkar18@vit.edu>
2020-07-11 18:19:25 +0530

    Added ArticleListTile

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
       title: 'Medium',
       theme: ThemeData(fontFamily: 'Nunito'),
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text("Home"),
           backgroundColor: Colors.black,
@@ -64,43 +65,86 @@ class HomePage extends StatelessWidget {
               color: Colors.grey.shade500,
             ),
             Expanded(
-              child: ListView(
-                children: <Widget>[
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-
-                        //*Details List
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Container(
-                                child: Text(
-                                  'Annoncing Flutter Linux Alpha with Canonical',
-                                  style: TextStyle(
-                                      fontSize: 15.0,
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                              ),
-                              Text('Chris Sells - 9 min read'),
-                              Icon(Icons.more_horiz)
-                            ],
-                          ),
-                        ),
-
-                        //*Image
-                        Image.asset('assets/images/placeholder.jpg',)
-                      ],
-                    ),
-                  )
-                ],
+              child: Container(
+                color: Colors.red,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 3,
+                  itemBuilder: (context, index) => ArticleListTile(),
+                ),
               ),
-            )
+            ),
+            Divider(
+              color: Colors.grey.shade500,
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ArticleListTile extends StatelessWidget {
+  const ArticleListTile({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: InkWell(
+        child: Container(
+          color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              //*Details List
+              Expanded(
+                child: Container(
+                  height: 100.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'A VIT Boy hacks GitHub and deletes all JS repos.',
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text('Chris Sells - 9 min read'),
+                      Icon(Icons.more_horiz)
+                    ],
+                  ),
+                ),
+              ),
+
+              //*Image
+              Padding(
+                padding: const EdgeInsets.only(left: 4.0),
+                child: Stack(
+                  children: [
+                    Image.asset(
+                      'assets/images/yash_square.jpg',
+                      height: 100.0,
+                      width: 100.0,
+                    ),
+                    Positioned(
+                      bottom: 0.0,
+                      child: Image.asset(
+                        'assets/images/placeholder.png',
+                        height: 25.0,
+                        width: 25.0,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );

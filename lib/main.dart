19eafdaef6e86d0587e6428b8057f9deb4a484a4
Yash <yash.halgaonkar18@vit.edu>
2020-07-11 22:12:58 +0530

    Started making Navigation Drawer.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +19,6 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Home"),
           backgroundColor: Colors.black,
-          leading: Icon(Icons.menu),
           actions: <Widget>[
             Icon(
               Icons.notifications_none,
@@ -28,6 +32,34 @@ class MyApp extends StatelessWidget {
             SizedBox(width: 15.0),
           ],
         ),
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              //*Header
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.white,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      CircleAvatar(),
+                      Text('Yash Halgaonkar'),
+                      Text('View Profile'),
+                    ],
+                  ),
+                ),
+              ),
+
+              Expanded(
+                flex: 5,
+                child: Container(
+                  color: Colors.grey.shade100,
+                ),
+              ),
+            ],
+          ),
+        ),
         body: HomePage(),
       ),
     );
@@ -37,7 +69,7 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   Widget _buildTitleWidget(String title) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 4.0),
+      padding: const EdgeInsets.only(bottom: 2.0),
       child: Text(
         'Your Daily Read',
         style: TextStyle(
@@ -49,91 +81,118 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  final dailyDigestList = [
+    DailyArticleTile(),
+    DailyArticleTile(),
+    DailyArticleTile(),
+  ];
+
+  final recomendadtionList = [
+    RecommendArticleTile(),
+    RecommendArticleTile(),
+    RecommendArticleTile(),
+    RecommendArticleTile(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            //*title
-            _buildTitleWidget('Your Daily Read'),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 0.0),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              //*title
+              _buildTitleWidget('Your Daily Read'),
 
-            //*Divider
-            Divider(
-              color: Colors.grey.shade500,
-            ),
-
-            //*ListView
-            Container(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: 3,
-                itemBuilder: (context, index) => ArticleListTile(),
+              //*Divider
+              Divider(
+                color: Colors.grey.shade500,
               ),
-            ),
 
-            //* Divider
-            Divider(
-              color: Colors.grey.shade500,
-            ),
+              //*DAILY DIGEST
+              ...dailyDigestList,
 
-            //*Tune your recommendations Widget
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Text(
-                      'Tune your recommendations',
-                      style: TextStyle(
-                          color: Colors.teal, fontWeight: FontWeight.w700),
-                    ),
-                    SizedBox(
-                      width: 15.0,
-                    ),
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(6.0)),
-                            border:
-                                Border.all(color: Colors.black, width: 1.0)),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Text(
-                                'Done for Today',
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 15.0,
-                                    fontWeight: FontWeight.w400),
-                              ),
-                              Icon(
-                                Icons.check_circle_outline,
-                                size: 20.0,
-                              )
-                            ],
-                          ),
+              //* Divider
+              Divider(
+                color: Colors.grey.shade500,
+              ),
+
+              //*Tune your recommendations Widget
+              RecommendationTile(
+                onTap: () {},
+              ),
+
+              //* Divider
+              Divider(
+                color: Colors.grey.shade500,
+              ),
+
+              ...recomendadtionList,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RecommendationTile extends StatelessWidget {
+  final Function onTap;
+
+  const RecommendationTile({
+    Key key,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(
+              'Tune your recommendations',
+              style: TextStyle(color: Colors.teal, fontWeight: FontWeight.w700),
+            ),
+            SizedBox(
+              width: 15.0,
+            ),
+            Expanded(
+              child: GestureDetector(
+                onTap: onTap,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                      border: Border.all(color: Colors.black, width: 1.0)),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 6.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Text(
+                          'Done for Today',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.w400),
                         ),
-                      ),
-                    )
-                  ],
+                        Icon(
+                          Icons.check_circle_outline,
+                          size: 20.0,
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
-
-            //* Divider
-            Divider(
-              color: Colors.grey.shade500,
-            ),
+            )
           ],
         ),
       ),
@@ -141,9 +200,9 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class ArticleListTile extends StatelessWidget {
+class DailyArticleTile extends StatelessWidget {
   final bool isBasesOn;
-  const ArticleListTile({
+  const DailyArticleTile({
     Key key,
     this.isBasesOn = false,
   }) : super(key: key);
@@ -151,72 +210,218 @@ class ArticleListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Column(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            isBasesOn ? Text('BASED ON YOUR READING HISTORY') : Container(),
-            isBasesOn
-                ? SizedBox(
-                    height: 8.0,
-                  )
-                : Container(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                //*Details List
-                Expanded(
-                  child: Container(
+            //*Details List
+            Expanded(
+              child: Container(
+                height: 90.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'A VIT Boy hacks GitHub and deletes all JS repos.',
+                      style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text('James Bond - 9 min read'),
+                    Icon(Icons.more_horiz)
+                  ],
+                ),
+              ),
+            ),
+
+            //*
+            SizedBox(
+              width: 10.0,
+            ),
+
+            //*Image
+            Padding(
+              padding: const EdgeInsets.only(left: 4.0),
+              child: Stack(
+                children: [
+                  Image.asset(
+                    'assets/images/yash_square.jpg',
                     height: 100.0,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    width: 80.0,
+                  ),
+                  Positioned(
+                    bottom: 0.0,
+                    child: Image.asset(
+                      'assets/images/placeholder.png',
+                      height: 25.0,
+                      width: 25.0,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RecommendArticleTile extends StatelessWidget {
+  const RecommendArticleTile({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //*Upper tile
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('BASED ON YOUR READING HISTORY'),
+                  SizedBox(
+                    height: 4.0,
+                  ),
+                  //*List tile
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(
-                          'A VIT Boy hacks GitHub and deletes all JS repos.',
-                          style: TextStyle(
-                              fontSize: 16.0,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold),
+                        //*Details List
+                        Expanded(
+                          child: Container(
+                            height: 120.0,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                //*Title
+                                Text(
+                                  'A VIT Boy hacks GitHub and deletes all JS repos.',
+                                  style: TextStyle(
+                                      fontSize: 16.0,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold),
+                                ),
+
+                                //* Summary
+                                Text(
+                                    'Yash Halgaonkar, 19, got so frustrated with JS that he decided to end it all.'),
+                                //Icon(Icons.more_horiz)
+                              ],
+                            ),
+                          ),
                         ),
-                        Text('Chris Sells - 9 min read'),
-                        Icon(Icons.more_horiz)
+
+                        //*
+                        SizedBox(
+                          width: 10.0,
+                        ),
+
+                        //*Image
+                        Padding(
+                          padding: const EdgeInsets.only(left: 4.0),
+                          child: Stack(
+                            children: [
+                              Image.asset(
+                                'assets/images/yash_square.jpg',
+                                height: 100.0,
+                                width: 80.0,
+                              ),
+                              Positioned(
+                                bottom: 0.0,
+                                child: Image.asset(
+                                  'assets/images/placeholder.png',
+                                  height: 25.0,
+                                  width: 25.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
                       ],
                     ),
                   ),
-                ),
-
-                //*
-                SizedBox(
-                  width: 10.0,
-                ),
-
-                //*Image
-                Padding(
-                  padding: const EdgeInsets.only(left: 4.0),
-                  child: Stack(
-                    children: [
-                      Image.asset(
-                        'assets/images/yash_square.jpg',
-                        height: 100.0,
-                        width: 100.0,
-                      ),
-                      Positioned(
-                        bottom: 0.0,
-                        child: Image.asset(
-                          'assets/images/placeholder.png',
-                          height: 25.0,
-                          width: 25.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
+                ],
+              ),
             ),
+
+            //*More options tile
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        //Text('David Anaya in Money Heist'),
+                        RichText(
+                          text: TextSpan(
+                            style: DefaultTextStyle.of(context).style,
+                            children: [
+                              TextSpan(
+                                text: 'James Bond',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black),
+                              ),
+                              TextSpan(
+                                text: ' in ',
+                                style: TextStyle(fontWeight: FontWeight.w300),
+                              ),
+                              TextSpan(
+                                text: 'Money Heist',
+                                style: TextStyle(fontWeight: FontWeight.w500),
+                              )
+                            ],
+                          ),
+                        ),
+                        Text('7/22/2020 - 4 min read')
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.bookmark_border,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          width: 15.0,
+                        ),
+                        Icon(
+                          Icons.more_vert,
+                          color: Colors.black87,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            SizedBox(
+              height: 4.0,
+            ),
+
+            Divider(
+              color: Colors.grey,
+            )
           ],
         ),
       ),
